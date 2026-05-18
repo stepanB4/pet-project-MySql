48a1822b7c08d89d8376ec1d33fdfc7c5c9c7305
DRaydenko <rda24f076@student.bmstu.ru>
2026-05-18 13:00:04 +0300

    d

--- app.py
+++ app.py
@@ -23,7 +23,8 @@ from database import (
 
 create_tables()
 app = FastAPI(title="Booking System")
-templates = Jinja2Templates(directory="templates")
+templates = Jinja2Templates(directory="templates", auto_reload=True)
+templates.env.cache = {}
 security = HTTPBasic()
 
 RUSSIAN_MONTHS = {
